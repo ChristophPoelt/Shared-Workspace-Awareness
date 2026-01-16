faa66046181f5fac71f9cd776884d63e1e464f9e
Franziska Augustinowski <f.augustinowski@campus.lmu.de>
2026-01-16 13:41:43 +0100

    main logic  extended

--- meeseeks/mainLogic.py
+++ meeseeks/mainLogic.py
@@ -3,24 +3,35 @@ from globalVariables import currentTargetGlobal
 
 #here we could handle the starting of all the nodes etc.
 
+#init function that:
+#subscribes to all needed nodes 
+#starts all needed nodes
+#-> Shiyi copies here already existing structure for the init function
+
+#bring robot in inital position (drive to 0.0 and correct arm position)
+#-> Franzi
+
 #select an inital target
 selectNewTarget
 
-#do the target selected gesture
-#do the target indication gesture (point towards target)
+#do the target selected gesture -> Shiyi
+
+#do the target indication gesture (point towards target) ->  Franzi
 
 #if loop that handles all the voice commands and then 
 #calls the according python script
 
-#pseudo code:
+#pseudo code: -> Christoph
 #if abort command was detected:
-#   call abort_command_logic.py
+#   call abort_command_logic
 #if pause command was detected:
-#   call pause_command_logic.py (needs to include doing the gesture)
+#   call pause_command_logic (needs to include doing the gesture)
 #if whereAreYouGoing is detected:
-#   call whereAreYouGoing_logic.py
+#   call whereAreYouGoing_logic
+#if continue is detected:
+#   call continue_logic
 
-#logic if target is reached
+#logic if target is reached -> we will figure it our later
 
 #select the next target
 selectNewTarget(currentTargetGlobal)
